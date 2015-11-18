d30b890f8cf232eff351093d57876ea5fc6cae3b
dcalvocasanova <dcalvocasanova@gmail.com>
2015-11-18 09:55:23 -0600

    Cambios appController
    Para el manejo del autenticador

--- cakephp-2.7.5/app/Controller/UsersController.php
+++ cakephp-2.7.5/app/Controller/UsersController.php
@@ -8,6 +8,11 @@ App::uses('AppController', 'Controller');
  */
 class UsersController extends AppController {
 
+	public function beforeFilter() {
+        parent::beforeFilter();
+        $this->Auth->allow('add');
+    }
+
 /**
  * Components
  *
