2c4a95a268986b52ed7a4d9e2ffa9d458a893a2f
MAURICIO ANTONIO RODRIGUEZ MORALES <mauricio.rodriguez_mora@ucr.ac.cr>
2015-11-24 13:59:30 -0600

    Edited the .htaccess. Redefined the User model. Init the config script.

--- cakephp-2.7.5/app/Model/User.php
+++ cakephp-2.7.5/app/Model/User.php
@@ -1,6 +1,6 @@
 <?php 
 App::uses('AppModel', 'Model');
-App::uses('AuthComponent', 'Controller/Component');
+App::uses('BlowfishPasswordHasher', 'Controller/Component/Auth');
 /**
  *
  * @property Snippet $Snippet
@@ -130,13 +130,13 @@ class User extends AppModel {
 	);
 	 
 	public function beforeSave($options = array()) {
-		// hash our password
 		if (isset($this->data[$this->alias]['password'])) {
-			$this->data[$this->alias]['password'] = AuthComponent::password($this->data[$this->alias]['password']);
+			$passwordHasher = new BlowfishPasswordHasher();
+			$this->data[$this->alias]['password'] = $passwordHasher->hash(
+				$this->data[$this->alias]['password']
+			);
 		}
-		
-		// fallback to our parent
-		return parent::beforeSave($options);
+		return true;
 	}
 }
 
